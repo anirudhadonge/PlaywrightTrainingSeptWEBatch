6894bc34009d0b68e6c07ff4414da78137edf881
Anirudha Donge <anirudha.donge@gmail.com>
2025-11-09 10:23:00 +0530

    update the

--- PlaywrightTraining/playwright.config.ts
+++ PlaywrightTraining/playwright.config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     
     browserName:'chromium',
-    channel:'msedge',
+    //channel:'msedge',
     headless:false,
     viewport:null,
     launchOptions:{
